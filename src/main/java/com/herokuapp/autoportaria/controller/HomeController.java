4bada8c43a591455a32dfbaf58c52a86c7f3d4de
Tiago Ferezin <tiagoferezin@gmail.com>
2018-12-17 00:57:34 -0200

    Index adicionado

---
+++ src/main/java/com/herokuapp/autoportaria/controller/HomeController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.herokuapp.autoportaria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Tiago Ferezin
+ * Data: 17/12/2018
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model){
+		return "home/index";
+	}
+	
+}
